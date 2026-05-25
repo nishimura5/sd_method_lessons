cbf4710c9ee109ca15d94119aff41e53c9ead5dc
nishimura5 <super.nishimura@gmail.com>
2026-05-25 11:16:55 +0900

    Refactor output display for unique values in sd_2.py to enhance readability

--- lessons/sd_2.py
+++ lessons/sd_2.py
@@ -16,9 +16,9 @@ print(f"ファイル名: {csv_file_path}\n行数: {num_rows}行")
 # "stimulus_id"カラムのユニークな値をstimulus_namesに格納して表示
 stimulus_names = src_df["stimulus_id"].unique()
 print("\n'stimulus_id' の一覧:")
-print(", ".join(stimulus_names))
+print(stimulus_names)
 
 # "respondent_id"カラムのユニークな値をsbj_namesに格納して表示
 sbj_names = src_df["respondent_id"].unique()
 print("\n'respondent_id' の一覧:")
-print(", ".join(sbj_names))
+print(sbj_names)
